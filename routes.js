017f2412f02d13faba89eb9ca64cf00301711a30
ThaigoDev <thaigodeveloper@gmail.com>
2024-10-10 09:54:16 -0300

    att

--- routes.js
+++ routes.js
@@ -5,7 +5,8 @@ const multerConfig = require("./src/config/multerConfig");
 const uploads = multer(multerConfig);
 const HomeController = require("./src/Controllers/HomeController"); 
 const CategoryController = require("./src/Controllers/CategoryController"); 
-const BusinessController = require("./src/Controllers/BusinessController"); 
+const BusinessController = require("./src/Controllers/BusinessController");   
+const HousingController = require("./src/Controllers/HousingController"); 
 const LoginAndSignUpController = require("./src/Controllers/LoginAndSignUpController")
 
 //nesse arquivo, vamos definir as rotas da nossa página, por exemplo : www.teste/[rotas que vamos criar]
@@ -15,11 +16,12 @@ router.post("/category/new",CategoryController.create);
 router.get("/admin/",LoginAndSignUpController.index); 
 router.post("/admin/auth",LoginAndSignUpController.auth); 
 router.get("/admin/logout",LoginAndSignUpController.logout); 
-
+//Comércio Routes
 router.get("/admin/business/",BusinessController.index);  
 router.post("/admin/business/new",uploads.single("businessPhoto"),BusinessController.create);  
 router.get("/admin/business/delete/:id",BusinessController.delete);  
 router.post("/admin/business/update/:id",uploads.single("businessPhotoEdited"),BusinessController.update); 
-
+//Moradia Routes
+router.get("/admin/housing/",HousingController.index); 
 
 module.exports = router; 
---
+++ src/Controllers/HousingController.js
@@ -0,0 +1,13 @@
+const BusinessRules = require("../ Models/BusinessModel");
+const CategoryRules = require("../ Models/CategoryModels");
+class HousingController {
+    static  async index(req,res) { 
+        const categoryBR = new CategoryRules(req.body);
+        const categories = await categoryBR.read();
+        const businessBR = new BusinessRules(req.body);
+        const allBusiness = await businessBR.read();
+        res.render("HousingPage",{categories,allBusiness}); 
+    }
+} 
+
+module.exports = HousingController;
