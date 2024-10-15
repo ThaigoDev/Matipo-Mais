003541b0adc3f4cb0774719ce88b7f55afe3a7d4
ThaigoDev <thaigodeveloper@gmail.com>
2024-10-15 20:35:47 -0300

    att

--- routes.js
+++ routes.js
@@ -11,17 +11,22 @@ const  BusinessHomeController = require("./src/Controllers/BusinessHomeControlle
 
 //nesse arquivo, vamos definir as rotas da nossa página, por exemplo : www.teste/[rotas que vamos criar]
 router.get("/",HomeController.index); //uma rota deve ter um controller, no caso  o controller vai linkar nosso banco de dados com o front end.   
-router.get("/business/index",BusinessHomeController.index); 
-router.get("/quemsomos/");  
-router.post("/category/new",CategoryController.create);  
+//rotas comércio 
+router.get("/admin/business/",BusinessController.index);  
+router.post("/admin/business/new",uploads.single("businessPhoto"),BusinessController.create);  
+router.get("/admin/business/delete/:id",BusinessController.delete);  
+router.post("/admin/business/update/:id",uploads.single("businessPhotoEdited"),BusinessController.update);  
+router.get("/business/index",BusinessHomeController.index);  
+//rotas quem somos 
+router.get("/quemsomos/");   
+//rotas categoria
+router.post("/category/new",CategoryController.create);   
+//rotas  página admin 
 router.get("/admin/",LoginAndSignUpController.index); 
 router.post("/admin/auth",LoginAndSignUpController.auth); 
 router.get("/admin/logout",LoginAndSignUpController.logout); 
 
-router.get("/admin/business/",BusinessController.index);  
-router.post("/admin/business/new",uploads.single("businessPhoto"),BusinessController.create);  
-router.get("/admin/business/delete/:id",BusinessController.delete);  
-router.post("/admin/business/update/:id",uploads.single("businessPhotoEdited"),BusinessController.update); 
+
 
 
 module.exports = router; 
